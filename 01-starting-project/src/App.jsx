5ecd55ed23c92b64bec29b43e6083ee6ce798cf6
ramyo564 <yohan032yohan@gmail.com>
2025-05-08 20:17:38 +0900

    1. project - List 데이터 동적 출력

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -34,10 +34,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concepItem) => (
+              <CoreConcept key={concepItem.title} {...concepItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
