9e8620be16abfb0afee35ca47358a5c41f3287eb
ramyo564 <yohan032yohan@gmail.com>
2025-05-08 19:55:32 +0900

    1. project - 버튼 스타일링

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
 export default function TabButton({ children, onSelect }) {
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className="active" onClick={onSelect}>{children}</button>
     </li>
   );
 }
