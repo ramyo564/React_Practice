a40ccbf0620e25b9a80edcc51d6c44c46da1ba85
ramyo564 <yohan032yohan@gmail.com>
2025-05-08 14:54:57 +0900

    1. project - props 사용하기

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,7 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import reactImage from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -8,7 +11,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -18,12 +21,41 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building bliock."
+              image={componentsImg}
+            />
+                        <CoreConcept
+              title="Components"
+              description="The core UI building bliock."
+              image={componentsImg}
+            />
+                        <CoreConcept
+              title="Components"
+              description="The core UI building bliock."
+              image={componentsImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
