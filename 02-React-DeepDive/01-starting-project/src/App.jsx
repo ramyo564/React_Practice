3975d929e83f1cde245f91d0c1f605d3eabb9365
ramyo564 <yohan032yohan@gmail.com>
2025-05-09 17:39:40 +0900

    2. React-DeepDive - Examples.jsx - 컴포넌트 상태 분리

--- 02-React-DeepDive/01-starting-project/src/App.jsx
+++ 02-React-DeepDive/01-starting-project/src/App.jsx
@@ -1,41 +1,14 @@
-import { useState, Fragment } from "react";
-
 import Header from "./components/Header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
-import { EXAMPLES } from "./data.js";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
-
-  console.log("APP COMPONENT EXECUTING");
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
     <>
       <Header />
       <main>
         <CoreConcepts />
-        
+        <Examples />
       </main>
     </>
   );
