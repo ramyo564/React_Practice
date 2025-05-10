b4e12b96e999d453f39603201430ce5e8b0c8626
ramyo564 <yohan032yohan@gmail.com>
2025-05-10 17:35:11 +0900

    3. TicTacToe - GameBoard 상태 끌어 올리기

--- 02-React-DeepDive/02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-React-DeepDive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -24,7 +24,8 @@ function App() {
           />
         </ol>
         <GameBoard
-
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
         />
       </div>
       LOG
