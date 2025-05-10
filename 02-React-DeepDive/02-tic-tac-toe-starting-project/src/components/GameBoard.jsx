18272508de4777207aad6496ce4e3e9e06211fce
ramyo564 <yohan032yohan@gmail.com>
2025-05-10 19:26:01 +0900

    3. TicTacToe - Props 속성에서 State 상태 파생하기
    이렇게 사용하면 공통된 상태를 유지할 수 있고
    각각의 상태를 병합할 필요가 없음
    상태관리에 있어서 매우 유용하다.

--- 02-React-DeepDive/02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 02-React-DeepDive/02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -4,20 +4,14 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare }) {
-//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-//   function handleSelectSquare(rowIndex, colIndex) {
-//     setGameBoard((prevGameBoard) => {
-//       const updatedBoard = [
-//         ...prevGameBoard.map((innerArray) => [...innerArray]),
-//       ];
-//       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-//       return updatedBoard;
-//     });
-
-//     onSelectSquare();
-//   }
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
 
   return (
     <ol id="game-board">
@@ -26,7 +20,7 @@ export default function GameBoard({ onSelectSquare }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
