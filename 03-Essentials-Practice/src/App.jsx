896b5e986d419040d6cc2ffce416e4968fe84d3a
ramyo564 <yohan032yohan@gmail.com>
2025-05-11 20:38:45 +0900

    4. Investment Calculator - 조건부 콘텐츠 출력

--- 03-Essentials-Practice/src/App.jsx
+++ 03-Essentials-Practice/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration >= 1;
   function handleChange(initialInvestment, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -25,7 +27,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than 0</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
