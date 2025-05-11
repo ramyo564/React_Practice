927aa69385d669d33279844f13b991eb66503c3b
ramyo564 <yohan032yohan@gmail.com>
2025-05-11 19:40:43 +0900

    4. Investment Calculator - 상태 끌어올리기 -> Results에 추후 상태 공유

--- 03-Essentials-Practice/src/App.jsx
+++ 03-Essentials-Practice/src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(initialInvestment, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [initialInvestment]: newValue,
+        // object["key"]
+        // object.key 는 직접 수정 => 불변성 꺠짐
+        // [key]: value <- 기존 키값과 벨류 값을 덮어씀
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
     </>
   );
 }
--- 03-Essentials-Practice/src/components/UserInput.jsx
+++ 03-Essentials-Practice/src/components/UserInput.jsx
@@ -1,25 +1,4 @@
-import { useState } from "react";
-
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  function handleChange(initialInvestment, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [initialInvestment]: newValue,
-        // object["key"]
-        // object.key 는 직접 수정 => 불변성 꺠짐
-        // [key]: value <- 기존 키값과 벨류 값을 덮어씀
-      };
-    });
-  }
-
+export default function UserInput({ onChange, userInput }) {
   return (
     <section id="user-input">
       <div className="input-group">
@@ -30,7 +9,7 @@ export default function UserInput() {
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              handleChange("initialInvestment", event.target.value)
+              onChange("initialInvestment", event.target.value)
             }
           />
         </p>
@@ -41,7 +20,7 @@ export default function UserInput() {
             required
             value={userInput.annualInvestment}
             onChange={(event) =>
-              handleChange("annualInvestment", event.target.value)
+              onChange("annualInvestment", event.target.value)
             }
           />
         </p>
@@ -53,9 +32,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.expectedReturn}
-            onChange={(event) =>
-              handleChange("expectedReturn", event.target.value)
-            }
+            onChange={(event) => onChange("expectedReturn", event.target.value)}
           />
         </p>
         <p>
@@ -64,7 +41,7 @@ export default function UserInput() {
             type="number"
             required
             value={userInput.duration}
-            onChange={(event) => handleChange("duration", event.target.value)}
+            onChange={(event) => onChange("duration", event.target.value)}
           />
         </p>
       </div>
