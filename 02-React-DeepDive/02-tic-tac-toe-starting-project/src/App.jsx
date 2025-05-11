6e4d3fcdbe2aa54d07d86093a233387ac26e9e3f
ramyo564 <yohan032yohan@gmail.com>
2025-05-11 12:09:55 +0900

    3. TicTacToe - 상태관리 간소화 및 불필요한 상태 분리
    리액트 코드는 상태는 최대한 적게사용하고
    최대한 많은 값을 파생 및 연산하도록 해야 좋다

--- 02-React-DeepDive/02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-React-DeepDive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -3,19 +3,24 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
-
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
@@ -41,7 +46,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- 02-React-DeepDive/02-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 02-React-DeepDive/02-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -2,7 +2,7 @@ export default function Log({ turns }) {
   return (
     <ol id="log">
       {turns.map((turn) => (
-        <li key={`${turns.square.row}${turn.square.col}`}>
+        <li key={`${turn.square.row}${turn.square.col}`}>
           {turn.player} selected {turn.square.row},{turn.square.col}
         </li>
       ))}
