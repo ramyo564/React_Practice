35206e33594232fb71ee5dbcd59609ed1a3e1d70
ramyo564 <yohan032yohan@gmail.com>
2025-05-11 15:56:25 +0900

    4. Investment Calculator - 초기설정

---
+++ 03-Essentials-Practice/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <h1>React Investment Calculator</h1>
+
+    // Add components for displaying a header,
+    // fetching user input & outputting the results table
+
+    // Fetch & store input (i.e.. the enterend investment parameters)
+
+    // Derive investment results with help of the provided utility function
+    // (in the starting project)
+
+    // Display investment results in a HTML table
+    // ( use <table> <thead> <tbody> <tr> <th> <td> )
+
+    // Conditionally display on info message
+    // if an invalid duration ( <1 ) was entered
+  );
+}
+
+export default App;
