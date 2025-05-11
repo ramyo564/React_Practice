f5d06f02a0f7d4bad74b173fdfef7502ae2a2876
ramyo564 <yohan032yohan@gmail.com>
2025-05-11 18:38:29 +0900

    4. Investment Calculator - 헤더컴포넌트

--- 03-Essentials-Practice/src/App.jsx
+++ 03-Essentials-Practice/src/App.jsx
@@ -1,20 +1,17 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import ResultTables from "./components/ResultTables";
+import UserTable from "./components/UserTable";
+
 function App() {
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
 
-    // Add components for displaying a header,
-    // fetching user input & outputting the results table
+    </>
 
-    // Fetch & store input (i.e.. the enterend investment parameters)
 
-    // Derive investment results with help of the provided utility function
-    // (in the starting project)
-
-    // Display investment results in a HTML table
-    // ( use <table> <thead> <tbody> <tr> <th> <td> )
-
-    // Conditionally display on info message
-    // if an invalid duration ( <1 ) was entered
   );
 }
 
