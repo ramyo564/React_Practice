c76012b5bacc828182eb539d052364c19fe83be2
ramyo564 <yohan032yohan@gmail.com>
2025-05-12 20:24:41 +0900

    05. Styling - 테일윈드 V4 커스텀 폰트 적용
    https://www.youtube.com/watch?v=sMWfU60bwfI&ab_channel=SREERAJM
    
    https://tailwindcss.com/docs/font-family

--- 05-Styling/src/components/Header.jsx
+++ 05-Styling/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         alt="A canvas"
         className="object-contain mb-8 w-44 h-44"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-pacifico">
         ReactArt
       </h1>
       <p>A community of artists and art-lovers.</p>
