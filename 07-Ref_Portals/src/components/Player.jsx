4dee7abd3cd2d3ab38ccd8ede70af8d7c050ce7b
ramyo564 <yohan032yohan@gmail.com>
2025-05-13 18:52:39 +0900

    7. Ref & Portals- 복습 -> State 상태를 이용한 사용자 입력 -> 양방향 바인딩

--- 07-Ref_Portals/src/components/Player.jsx
+++ 07-Ref_Portals/src/components/Player.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handlClick() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button >Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handlClick}>Set Name</button>
       </p>
     </section>
   );
