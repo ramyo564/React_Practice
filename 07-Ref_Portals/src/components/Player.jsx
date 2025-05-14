85f158b1ae7f71a78047b7648f224ddb85001af2
ramyo564 <yohan032yohan@gmail.com>
2025-05-14 15:59:10 +0900

    7. Ref & Portals - 규칙위반

--- 07-Ref_Portals/src/components/Player.jsx
+++ 07-Ref_Portals/src/components/Player.jsx
@@ -7,6 +7,11 @@ export default function Player() {
 
   function handlClick() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+    // 규칙 위반!
+    // 직접 DOM을 조작해서 UI를 바꾸지 말라는 뜻
+    // 왜냐면 리액트가 그걸 자동으로 해주는데 
+    // DOM을 직접 건드리면 리액트의 내부 상태와 실제 DOM 상태가 불일치할 수 있기 때문
   }
   return (
     <section id="player">
