0e8dc7142347d7a167e29f77778a399560d0986d
ramyo564 <yohan032yohan@gmail.com>
2025-05-14 21:43:54 +0900

    7. Ref & Portals - 점수 계산 출력

--- 07-Ref_Portals/src/components/ResultModal.jsx
+++ 07-Ref_Portals/src/components/ResultModal.jsx
@@ -8,6 +8,7 @@ const ResultModal = forwardRef(function ResultModal(
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => ({
     open() {
@@ -18,6 +19,7 @@ const ResultModal = forwardRef(function ResultModal(
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score: {score} </h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
