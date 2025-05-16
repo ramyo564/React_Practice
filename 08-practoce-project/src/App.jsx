aeab7864311fac1b990fe48eefea1f20672a3b62
ramyo564 <yohan032yohan@gmail.com>
2025-05-16 21:18:38 +0900

    8. Practice - 컴포넌트간 교체를 위한 상태 관리법

--- 08-practoce-project/src/App.jsx
+++ 08-practoce-project/src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartedAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartedAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      {/* <NewProject/> */}
-      <NoProjectSelected/>
+      <ProjectSidebar onStartAddProject={handleStartedAddProject} />
+      {content}
     </main>
   );
 }
