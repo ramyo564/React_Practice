62df1ef8ede670c3df30589b2afd238af9b5a455
ramyo564 <yohan032yohan@gmail.com>
2025-05-16 19:02:01 +0900

    8. Practice - 사이드바 컴포넌트

---
+++ 08-practoce-project/src/components/ProjectSidebar.jsx
@@ -0,0 +1,15 @@
+export default function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul>
+        <li>dd</li>
+        <li>dd</li>
+        <li>dd</li>
+      </ul>
+    </aside>
+  );
+}
