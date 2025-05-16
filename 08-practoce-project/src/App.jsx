5fe742ea74340c321ffb68163d871f89bd461ccc
ramyo564 <yohan032yohan@gmail.com>
2025-05-16 21:53:32 +0900

    8. Practice - 프로젝트 생성 핸들링 & UI 업데이트

--- 08-practoce-project/src/App.jsx
+++ 08-practoce-project/src/App.jsx
@@ -46,7 +46,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartedAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartedAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
