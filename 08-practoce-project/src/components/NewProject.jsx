795b14a69eb9432e675189bbd68cb782e618a23d
ramyo564 <yohan032yohan@gmail.com>
2025-05-16 22:34:25 +0900

    8. Practice - 테일윈드 css 모달 스타일링

--- 08-practoce-project/src/components/NewProject.jsx
+++ 08-practoce-project/src/components/NewProject.jsx
@@ -16,8 +16,8 @@ export default function NewProject({ onAdd }) {
 
     if (
       enteredTitle.trim() === "" ||
-      enteredDescription.trim === "" ||
-      enteredDueDate.trim === ""
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
     ) {
       modal.current.open();
       return;
@@ -31,10 +31,14 @@ export default function NewProject({ onAdd }) {
 
   return (
     <>
-      <Modal ref={modal} button="Close">
-        <h2>Invalid Input</h2>
-        <p>Oops ... looks like you forgot to enter a value.</p>
-        <p>Please make sure you provide a valid value for every input field.</p>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
