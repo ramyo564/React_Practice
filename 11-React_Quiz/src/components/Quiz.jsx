8e0356da0376c3f7fa4dfa3811fc06f2182a1aec
ramyo564 <yohan032yohan@gmail.com>
2025-05-19 15:21:19 +0900

    11. React_Quiz - 퀴즈 질문 및 답변 등록

--- 11-React_Quiz/src/components/Quiz.jsx
+++ 11-React_Quiz/src/components/Quiz.jsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
-
+import QUESTIONS from "../questions.js";
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <p>{QUESTIONS[activeQuestionIndex].text}</p>;
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
