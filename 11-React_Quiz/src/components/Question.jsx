088874083f11e853e761f8b2e44715cea3718d5b
ramyo564 <yohan032yohan@gmail.com>
2025-05-19 20:39:03 +0900

    11. React_Quiz - Key 문제 다시 발생
    props로 받는 key는 리엑트에서 단독으로 사용하기 때문에 해당 방식으로 사용하면 안된다.

--- 11-React_Quiz/src/components/Question.jsx
+++ 11-React_Quiz/src/components/Question.jsx
@@ -1,23 +1,46 @@
+import { useState } from "react";
 import Answers from "./Answers";
 import QuestionTimer from "./QuestionTimer";
+import QUESTIONS from "../questions.js";
 
-export default function Question({
-  questionText,
-  answers,
-  onSelectAnswer,
-  selectedAnswer,
-  answerState,
-  onSkipAnswer,
-}) {
+export default function Question({ key, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[key].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+
+  if (answer.selectedAnswer) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  }
+
   return (
     <div id="question">
       <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
-      <p>{questionText}</p>;
+      <p>{QUESTIONS[key].text}</p>;
       <Answers
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[key].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
