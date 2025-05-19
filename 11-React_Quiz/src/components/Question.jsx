9c7b27224f142ffa2beb1c6d1e1e6daaf32ceab6
ramyo564 <yohan032yohan@gmail.com>
2025-05-19 20:11:21 +0900

    11. React_Quiz - key 문제 해결
    컴포넌트를 나눠서 해당 문제있는 컴포넌트를 묶어버리면 key 한 개로 모두 동시에 컴포넌트 재실행이 가능하다.

---
+++ 11-React_Quiz/src/components/Question.jsx
@@ -0,0 +1,24 @@
+import Answers from "./Answers";
+import QuestionTimer from "./QuestionTimer";
+
+export default function Question({
+  questionText,
+  answers,
+  onSelectAnswer,
+  selectedAnswer,
+  answerState,
+  onSkipAnswer,
+}) {
+  return (
+    <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <p>{questionText}</p>;
+      <Answers
+        answers={answers}
+        selectedAnswer={selectedAnswer}
+        answerState={answerState}
+        onSelect={onSelectAnswer}
+      />
+    </div>
+  );
+}
--- 11-React_Quiz/src/components/Quiz.jsx
+++ 11-React_Quiz/src/components/Quiz.jsx
@@ -1,8 +1,7 @@
 import { useCallback, useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
-import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
   const [answerState, setAnswerState] = useState("");
@@ -48,21 +47,15 @@ export default function Quiz() {
 
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={10000}
-          onTimeout={handleSkipAnswer}
-        />
-        <p>{QUESTIONS[activeQuestionIndex].text}</p>;
-        <Answers
-          key={activeQuestionIndex}
-          answers={QUESTIONS[activeQuestionIndex].answers}
-          selectedAnswer={userAnswers[userAnswers.length - 1]}
-          answerState={answerState}
-          onSelect={handleSelectAnswer}
-        />
-      </div>
+      <Question
+        key={activeQuestionIndex}
+        questionText={QUESTIONS[activeQuestionIndex].text}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        answerState={answerState}
+        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
