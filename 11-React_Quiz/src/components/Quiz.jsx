ab3568a7605ad4f5f5b57bbef95a86504c811fe1
ramyo564 <yohan032yohan@gmail.com>
2025-05-19 16:44:56 +0900

    11. React_Quiz - 퀴즈 답안 순서 섞어서 출력

--- 11-React_Quiz/src/components/Quiz.jsx
+++ 11-React_Quiz/src/components/Quiz.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  suffledAnswers.sort(() => Math.random() - 0.5);
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -15,7 +18,7 @@ export default function Quiz() {
       <div id="question">
         <p>{QUESTIONS[activeQuestionIndex].text}</p>;
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {suffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
