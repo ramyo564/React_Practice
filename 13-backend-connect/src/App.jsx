bc9405344505ca8a6a64c8adc563a8d38f6a76c4
ramyo564 <yohan032yohan@gmail.com>
2025-05-24 19:13:37 +0900

    13. Backend Connecting - Optimistic update 낙관적 업데이트
    이제 GET뿐만 아니라 POST/PUT 같은 데이터 전송도 가능해짐
    근데 주의할 점 있음
    
    예를 들어, loading 상태 안 씀
    보통은 loading, success, error 이렇게 세 가지 상태로 관리함
    
    대신 낙관적 업데이트 사용함 (optimistic update)
    서버 응답 기다리지 않고, 먼저 UI부터 바꿔버림
    
    장소 선택하면 바로 화면에 반영됨
    서버 요청은 그 후에 감
    
    요청 실패하면 catch에서 예전 상태로 롤백함
    그래서 옛날 userPlaces를 기억하고 있어야 함
    
    상태 업데이트 함수 형식 (setX(prev => ...)) 안 씀
    이유는 실패했을 때 복구할 정확한 상태가 필요하기 때문임
    
    이게 낙관적 업데이트의 핵심
    사용자 입장에선 빠르게 반응해서 체감 좋음
    
    물론 실패하면 뭔가 이상해질 수 있음
    그래서 실패했을 때 보여줄 에러 상태도 따로 만들어둠 (errorUpdatingPlaces)
    
    에러 생기면 메시지 담아서 상태에 저장함
    이 메시지로 모달 띄움
    
    닫을 수 있어야 하니까 handleError 함수로 상태 초기화도 가능하게 함
    모달은 errorUpdatingPlaces 있을 때만 렌더링되게 조건 설정함
    
    안 그러면 없는 메시지 접근해서 에러남
    마지막엔 테스트로 일부러 URL 망가뜨려 에러 발생시킴
    
    에러 뜨고, 모달로 사용자에게 알려줌
    모달 닫으면 상태 초기화됨
    
    낙관적 업데이트와 에러 처리 방법까지 모두 다룸

--- 13-backend-connect/src/App.jsx
+++ 13-backend-connect/src/App.jsx
@@ -6,11 +6,14 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import { updateUserPlaces } from "./http.js";
+import ErrorPage from "./components/ErrorPage.jsx";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+
+  const [errorUdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -37,7 +40,10 @@ function App() {
     try {
       await updateUserPlaces([selectedPlace, ...userPlaces]);
     } catch (error) {
-      //...
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || "Failed to update places.",
+      });
     }
   }
 
@@ -49,8 +55,22 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
+
   return (
     <>
+      <Modal open={errorUdatingPlaces} onClose={handleError}>
+        {errorUdatingPlaces && (
+          <ErrorPage
+            title="An error occurred!"
+            message={errorUdatingPlaces.message}
+            onConfirm={handleError}
+          />
+        )}
+      </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
