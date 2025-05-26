51dcb9b1a9e0d7b91da71a8c8115e80234e548ae
ramyo564 <yohan032yohan@gmail.com>
2025-05-26 17:42:39 +0900

    16. Form Actions React 19 - 유효성 검사 로직 추가
    📌 1. 폼 데이터 추출하기
    ✅ 변수 간결화
        formData.get('email') → email 상수로 저장 (간결하게 하기 위함)
    
    ✅ 입력 필드에서 값 추출 (각 필드의 name 속성 기준)
        password: formData.get('password')
         ...
         ...
        acquisitionChannel: formData.getAll('acquisition')
        → 여러 체크박스 선택값을 배열로 추출
    
    📌 2. 입력값 검증하기
    ✅ 준비 작업
    
        errors 배열 선언
    
        utils/validation.js 파일에서 헬퍼 함수 4개 import:
    
            isEmail, isNotEmpty, isEqualToOtherValue, hasMinLength
    
    ✅ 개별 검증 로직 추가
        각각의 유효성 검사
    
    📌 3. 정리
        사용자는 필드를 채우지 않고 폼을 제출할 수 있음 → 오류 메시지 필요
        추출한 값을 바탕으로 유효성 검사 수행
        오류 발생 시 errors 배열에 메시지 추가

--- 16-Form-Actions-React-19/src/components/Signup.jsx
+++ 16-Form-Actions-React-19/src/components/Signup.jsx
@@ -1,7 +1,47 @@
+import {
+  isEmail,
+  isNotEmpty,
+  isEqualToOtherValue,
+  hasMinLength,
+} from "../util/validation.js";
+
 export default function Signup() {
   function signupAction(formData) {
-    const enteredEmail = formData.get("email");
-    console.log(enteredEmail);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm-password");
+    const firstName = formData.get("first-name");
+    const lastName = formData.get("last-name");
+    const role = formData.get("role");
+    const terms = formData.get("terms");
+    const acquisitionChannel = formData.getAll("acquisition");
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push("Invalid email address.");
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push("You must provide a password with at least six characters");
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push("Passwords do not match.");
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("Please provide both your first and last name.");
+    }
+    if (!isNotEmpty(role)) {
+      errors.push("Plase select a role.");
+    }
+    if (!terms) {
+      errors.push("You must agree to the terms and conditions");
+    }
+    if (acquisitionChannel.length === 0) {
+      errors.push("Please select at least one acquisition channel.");
+    }
   }
   return (
     <form action={signupAction}>
