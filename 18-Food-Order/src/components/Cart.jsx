3dd2594b03be749ae2b26db5c58900e8bf465562
ramyo564 <yohan032yohan@gmail.com>
2025-05-30 15:11:15 +0900

    18. Food Order - CartItem 컴포넌트 분리
    ✅ 이번 목표
        장바구니 항목 UI 구성
        JSX를 Cart 컴포넌트에 직접 추가하거나, 별도의 CartItem 컴포넌트로 분리 가능
    
        여기서는 CartItem 컴포넌트를 따로 생성하는 방식 선택
    
    ✅ CartItem 컴포넌트 구현
    
        기본 구조
    
            목록 항목(<li className="cart-item">)으로 JSX 반환
    
            항목 내용: 이름 + 수량 * 가격
    
            p 태그로 수량 조절 버튼 제공
    
        스타일링
    
            항목 전체에 cart-item 클래스
    
            버튼이 있는 문단에 cart-item-actions 클래스
    
        Props 처리
    
            name, quantity, price 를 props로 전달받음
    
            가격은 currencyFormatter를 활용해 포맷팅
    
    ✅ 버튼 동작 로직 연결
    
        기존 컨텍스트 사용 X
        → 컴포넌트 재사용성을 높이기 위해 부모로부터 함수 props로 전달
    
        props로 받은 함수 사용
    
            onIncrease: 수량 +1 버튼
    
            onDecrease: 수량 -1 버튼
    
            각각 onClick에 연결
    
    ✅ Cart 컴포넌트에서 CartItem 사용
    
        컴포넌트 import & 사용
    
            item.id를 key로 지정
    
            ...item을 사용해 name, quantity, price 등 props 전달
    
        onIncrease / onDecrease 함수 전달
    
            onIncrease: cartCtx.addItem(item) 실행
    
            onDecrease: cartCtx.removeItem(item.id) 실행
    
    ✅ 결과
    
        수량 증가/감소 버튼 정상 작동
    
        버튼 클릭 시 실제로 수량이 변경됨

--- 18-Food-Order/src/components/Cart.jsx
+++ 18-Food-Order/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import Button from "./UI/Button.jsx";
 import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -23,9 +24,12 @@ export default function Cart() {
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            {...item}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDescrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
