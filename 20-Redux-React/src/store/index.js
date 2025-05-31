98b7f88166a250c48fa6109439a5395db05359b8
ramyo564 <yohan032yohan@gmail.com>
2025-05-31 15:44:04 +0900

    20. Redux React - redux store 생성

---
+++ 20-Redux-React/src/store/index.js
@@ -0,0 +1,23 @@
+// import { createStore } from "redux"; <-- depracated!
+
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+export default store;
