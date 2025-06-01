cdc7a5b4f32761162d810a928bb2542f0a3b25c9
ramyo564 <yohan032yohan@gmail.com>
2025-06-01 18:21:15 +0900

    21. Redux Deep - 필요없는 props 삭제

--- 21-Redux-Deep/src/components/Cart/CartButton.js
+++ 21-Redux-Deep/src/components/Cart/CartButton.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import classes from "./CartButton.module.css";
 import { uiActions } from "../../store/ui-slice";
 
-const CartButton = (props) => {
+const CartButton = () => {
   const dispatch = useDispatch();
   const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const toggleCartHandler = () => {
